4f8f38821789a5a72ee26a73297eb87884b4946a
Lannion <83488325+Lannion@users.noreply.github.com>
2025-01-22 04:15:53 +0800

    Update settings.py

--- enrollment/settings.py
+++ enrollment/settings.py
@@ -58,7 +58,8 @@ ASGI_APPLICATION = 'enrollment.asgi.application'  # For asynchronous support
 # CORS settings to allow frontend to communicate
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "backend-production-7f56.up.railway.app", 
+    "backend-production-7f56.up.railway.app",
+    "frontend-qd0b.onrender.com", 
 ]
 CORS_ALLOW_CREDENTIALS = True  # Enable credentials (cookies)
 
